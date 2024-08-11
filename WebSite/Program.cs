4a91ed422b7fcd3c5e4338fc6b598ba186cef6bd
NR <95234863+sraww@users.noreply.github.com>
2024-08-11 11:40:16 +0300

    Update Program.cs
    fix

--- WebSite/Program.cs
+++ WebSite/Program.cs
@@ -12,10 +12,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
